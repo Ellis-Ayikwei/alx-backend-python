c861891389d2e11bba074579384684eb0ec49c1a
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-01 18:34:52 +0000

    "Added docstring to add function"

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -6,4 +6,16 @@ returns their sum as a float.
 
 
 def add(a: float, b: float) -> float:
+    """
+    Calculates the sum of two floating point numbers.
+    
+    Args:
+        a (float): The first number to be added.
+        b (float): The second number to be added.
+    
+    Returns:
+        float: The sum of the two numbers.
+    """
+    
+    # Add the two numbers and convert the result to a float.
     return float(a + b)
