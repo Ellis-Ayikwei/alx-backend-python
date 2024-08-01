e3d96525c782a763e3fb58ea09ce94e17189dfc4
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-01 22:01:51 +0000

    to dtr

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""function to_str that takes a float n as argument and returns the string
+representation of the float."""
+
+
+def to_str(n:  float) -> str:
+    """
+    Returns the string representation of a float number.
+
+    Args:
+        n (float): The float number to convert to a string.
+
+    Returns:
+        str: The string representation of the float number.
+    """
+    return str(n)
