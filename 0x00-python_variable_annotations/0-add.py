58351168328fc6fee13d9640aa70ba92923f6571
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-01 18:36:29 +0000

    "Updated shebang line and reformatted docstring in add.py"

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 function add that takes a float a and a float b as arguments and
 returns their sum as a float.
@@ -8,14 +8,12 @@ returns their sum as a float.
 def add(a: float, b: float) -> float:
     """
     Calculates the sum of two floating point numbers.
-    
+
     Args:
         a (float): The first number to be added.
         b (float): The second number to be added.
-    
+
     Returns:
         float: The sum of the two numbers.
     """
-    
-    # Add the two numbers and convert the result to a float.
     return float(a + b)
