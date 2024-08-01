264d75b829e74207aecef39b65fc2674d2b6e505
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-01 22:36:03 +0000

    "Added docstring and variable to to_kv function, reformatted code"

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,9 +4,22 @@ float v as arguments and returns a tuple. The first element of the tuple
 is the string k. The second element is the square of the int/float v and
 should be annotated as a float.
 """
-
 from typing import Tuple, List, Union
 
 
 def to_kv(key: str, value: Union[int, float]) -> Tuple[str, float]:
-    return (key, value ** 2)
+    """
+    This function takes a string key and a value that can be an int or a float.
+    It returns a tuple where the first element is the key and the
+    second element
+    is the square of the value.
+
+    :param key: A string key.
+    :param value: An int or float value.
+    :return: A tuple with the key and the square of the value.
+    """
+    # Square the value
+    squared_value = value ** 2
+
+    # Return a tuple with the key and the squared value
+    return (key, squared_value)
