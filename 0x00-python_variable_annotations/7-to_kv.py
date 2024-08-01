1277208d632883b816d34143cd4a924c9388ae47
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-01 22:45:50 +0000

    "Refactor docstring and simplify variable assignment in to_kv function"

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -9,14 +9,16 @@ from typing import Tuple, List, Union
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    This function takes a string key and a value that can be an int or a float.
-    It returns a tuple where the first element is the key and the second element
+    This function takes a string key and a value that can be an int or
+    a float.
+    It returns a tuple where the first element is the key and the
+    second element
     is the square of the value.
 
     :param key: A string key.
     :param value: An int or float value.
     :return: A tuple with the key and the square of the value.
     """
-    v =  v * v
+    v = v*v
 
     return k, v
