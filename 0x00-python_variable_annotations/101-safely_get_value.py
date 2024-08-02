ae671a742d9c537f15d705e9ff3070684d978cf1
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-02 00:37:00 +0000

    Remove docstring parameters description in safely_get_value function.

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -14,12 +14,6 @@ def safely_get_value(dct: Mapping, key: Any,
     """
     Safely get a value from a dictionary, returning a default value
     if the key is not found.
-
-    :param dct: Dictionary from which to retrieve the value.
-    :param key: Key whose value is to be retrieved.
-    :param default: Default value to return if the key is not found.
-    :return: The value associated with the key if it exists, otherwise
-    the default value.
     """
     if key in dct:
         return dct[key]
