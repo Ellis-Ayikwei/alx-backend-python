0d3136afd4efe97d4d6a66f6395de436245cd485
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-02 01:12:30 +0000

    "Deleted multiple Python files in the 0x00-python_variable_annotations directory."

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -15,13 +15,6 @@ def safely_get_value(dct: Mapping, key: Any,
     """
     Safely get a value from a dictionary, returning a default value
     if the key is not found.
-
-    :param dct: A mapping (dictionary) from which to retrieve the value.
-    :param key: The key whose value is to be retrieved.
-    :param default: The default value to return if the key is not found.
-    Default is None.
-    :return: The value associated with the key if it exists, otherwise
-    the default value.
     """
     if key in dct:
         return dct[key]
