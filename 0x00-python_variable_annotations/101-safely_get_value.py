85a26fd592e6d9c57ef414dd3a527da037113657
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-02 00:44:52 +0000

    Remove type hint for Mapping in safely_get_value function.

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,7 +8,8 @@ returning a default value if the key is not found.
 
 T = TypeVar('T')
 
-def safely_get_value(dct: Mapping[Any, Any], key: Any,
+
+def safely_get_value(dct: Mapping, key: Any,
                      default: Optional[T] = None) -> Union[Any, T]:
     """
     Safely get a value from a dictionary, returning a default value
