fb802bbbf7abf3fe9e8ca296e4e4d5c52135cc64
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-02 00:01:52 +0000

    "Added docstring to 100-safe_first_element.py"

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+This module defines a function that safely returns the first element of a sequence
+if it exists, otherwise returns None.
+"""
 from typing import Sequence, Any, Union
 
 
