cb56141abaa6b95a434d9e0e8a1a353894886d90
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-02 01:13:41 +0000

    "Added docstring to safely_get_value function and reformatted imports"

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-from typing import TypeVar, Union, Optional, Any, Mapping
-
 """
 This module defines a function that safely retrieves a value
 from a dictionary,
 returning a default value if the key is not found.
 """
+from typing import TypeVar, Union, Optional, Any, Mapping
 
 T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Optional[T] = None) -> Union[Any, T]:
-    
+    """
+    Safely get a value from a dictionary, returning a default value
+    if the key is not found.
+    """
     if key in dct:
         return dct[key]
     else:
