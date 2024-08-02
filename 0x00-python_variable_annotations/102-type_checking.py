93138cd3a13f9e1177243e381415dad186ffa282
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-02 01:28:44 +0000

    Refactor zoom_array function to remove unnecessary comments and improve code readability

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -8,13 +8,6 @@ from typing import Tuple, List, Union
 def zoom_array(lst: Union[Tuple, List], factor: int = 2) -> Union[Tuple, List]:
     """
     Zooms in on a list or tuple.
-
-    Args:
-        lst (Union[Tuple, List]): The input list or tuple to be zoomed in on.
-        factor (int): The factor by which to zoom in. Default is 2.
-
-    Returns:
-        Union[Tuple, List]: A zoomed-in list or tuple.
     """
     zoomed_in: List = [
         item for item in lst
