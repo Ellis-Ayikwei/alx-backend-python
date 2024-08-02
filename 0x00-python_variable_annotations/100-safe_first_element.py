cb71451cd81623b0e30416710a974cc5cb8ca0ae
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-02 00:00:55 +0000

    "Added a blank line between import statements and function definition."

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from typing import Sequence, Any, Union
 
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
     Returns the first element of a sequence if it exists,
