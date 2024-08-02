fc54f26b483db462c0ba6fced6bcd9983dc8daed
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-02 00:00:14 +0000

    "Removed shebang comment, reformatted docstring, and removed unnecessary parameter description"

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,18 +1,10 @@
-#!/usr/bin/env pyhton3
-"""
-This module defines a function that safely returns the first element of a
-sequence or None if the sequence is empty.
-"""
+#!/usr/bin/env python3
 from typing import Sequence, Any, Union
-
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
     Returns the first element of a sequence if it exists,
     otherwise returns None.
-
-    :param sequence: A sequence of elements.
-    :return: The first element of the sequence if it exists, otherwise None.
     """
     if lst:
         return lst[0]
