411afd243c585b14c001d212b897bbe496016722
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-31 20:11:27 +0000

    Refactor TestMemoize class for improved code readability and remove unnecessary comments

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -75,29 +75,24 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """ test class to tes utils.memoize"""
-
+    """Test memoize decorator"""
     def test_memoize(self):
-        """ Tests the function when calling a_property twice,
-        the correct result is returned but a_method is only
-        called once using assert_called_once
-        """
-
+        """Test memoize decorator"""
         class TestClass:
             """ Test Class for wrapping with memoize """
-
+             
             def a_method(self):
                 return 42
 
             @memoize
-            def a_property(self):
+            def a_property(self) :
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method') as mock:
-            test_class = TestClass()
-            test_class.a_property()
-            test_class.a_property()
-            mock.assert_called_once()
+        with patch.object(TestClass, 'a_method') as mock_a_method:
+            instance = TestClass()
+            instance.a_property()
+            instance.a_property()
+            mock_a_method.assert_called_once()
 
 
 if __name__ == '__main__':
