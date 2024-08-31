0bc48199ede27a33dde3f903d4d7b0886ed043e2
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-31 03:30:10 +0000

    Refactor test_access_nested_map function for improved code readability and removed unnecessary comments

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,8 +21,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), 2),
         ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
-    def test_access_nested_map(self, nested_map: Mapping[str, Any],
-                               path: Sequence[str], expected_outcome: Any) -> None:
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected_outcome: Any) -> None:
         """Test access_nested_map function
 
         Args:
@@ -35,6 +35,28 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(self.access_nested_map(nested_map, path),
                          expected_outcome)
+
+
+    @parameterized.expand([
+        # Test with empty dict
+        ({}, ("a",)),
+        # Test with a single key
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                               path: Sequence) -> None:
+        """Test that a KeyError is raised when a key does not exist in the
+        nested map
+
+        Args:
+            nested_map (Mapping[str, Any]): A dictionary with nested values
+            path (Sequence[str]): A sequence of keys used to access a value
+
+        Returns:
+            None
+        """
+        with self.assertRaises(KeyError):
+            self.access_nested_map(nested_map, path)
 
 
 if __name__ == '__main__':
