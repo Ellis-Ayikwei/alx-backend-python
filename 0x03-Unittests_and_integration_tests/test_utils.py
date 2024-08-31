5507e1d057cfdbcb12c02455183c05a604bb05ba
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-31 03:15:50 +0000

    Refactor test_access_nested_map function for improved code readability and removed unnecessary comments

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,9 +21,18 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), 2),
         ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence,
-                               expected_outcome: Any) -> Any:
-        """Test access_nested_map function"""
+    def test_access_nested_map(self, nested_map: Mapping[str, Any],
+                               path: Sequence[str], expected_outcome: Any) -> None:
+        """Test access_nested_map function
+
+        Args:
+            nested_map (Mapping[str, Any]): A dictionary with nested values
+            path (Sequence[str]): A sequence of keys used to access a value
+            expected_outcome (Any): The expected outcome of the function
+
+        Returns:
+            None
+        """
         self.assertEqual(self.access_nested_map(nested_map, path),
                          expected_outcome)
 
