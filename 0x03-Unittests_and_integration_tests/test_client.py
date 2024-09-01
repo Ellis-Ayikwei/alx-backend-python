13ca985cd67c07bb9110e23c47725ae060434394
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-01 02:17:51 +0000

    Refactor TestMemoize class for improved code readability and remove unnecessary comments

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,23 +11,31 @@ from utils import (
 )
 
 
-class TestGithubOrgClient(unittest.TestCase):
-    """Tests the `GithubOrgClient` class."""
+class TestGitHubOrgClient(unittest.TestCase):
+    """Defines a class to test the GitHUbOrgClient in the Client Module"""
+
     @parameterized.expand([
-        ("google", {'login': "google"}),
-        ("abc", {'login': "abc"}),
+        ('google', {'login': 'google'}),
+        ('abc', {'login': 'abc'}),
     ])
-    @patch(
-        "client.get_json",
-    )
-    def test_org(self, org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
-        """Tests the `org` method."""
-        mocked_fxn.return_value = MagicMock(return_value=resp)
-        gh_org_client = GithubOrgClient(org)
-        self.assertEqual(gh_org_client.org(), resp)
-        mocked_fxn.assert_called_once_with(
-            "https://api.github.com/orgs/{}".format(org)
-        )
+    @patch('client.get_json')
+    def test_org(self, org_name: str, expected_result: Dict[str, str],
+                 mock: MagicMock) -> None:
+        """Test org method
+
+        Args:
+        org_name (str): The organization name
+        expected_result (Dict[str, str]): The expected result
+        mock (unittest.mock.MagicMock): The mock object
+
+        Returns:
+        None
+        """
+        mock.return_value = MagicMock(return_value=expected_result)
+        org_client = GithubOrgClient(org_name)
+        self.assertEqual(org_client.org(), expected_result)
+        mock.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
 
 
 if __name__ == '__main__':
