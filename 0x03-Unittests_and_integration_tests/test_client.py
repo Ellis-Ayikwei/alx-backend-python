c2d370d3e0f700cb9a3c13b62a8428da377724d5
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-01 03:17:01 +0000

    Refactor TestMemoize class for improved code readability and remove unnecessary comments

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,8 @@
 """Defines tests for the client module"""
 import unittest
 from typing import Dict
-from unittest.mock import patch, MagicMock
+from unittest import mock
+from unittest.mock import PropertyMock, patch, MagicMock
 from parameterized import parameterized
 from client import GithubOrgClient
 import utils
@@ -19,14 +20,25 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc", {"login": "abc"}),
     ])
     @patch("client.get_json")
-    def test_org(self, org: str, expected_result: Dict, mock_get_json: MagicMock) -> None:
+    def test_org(self, org: str, expected_result: Dict,
+                 mock_get_json: MagicMock) -> None:
         """Tests the `org` method."""
-        mock_get_json.return_value = expected_result  # Directly return the expected result
+        mock_get_json.return_value = expected_result
 
         gh_org_client = GithubOrgClient(org)
-        self.assertEqual(gh_org_client.org, expected_result)  # Make sure to access the property directly
+        self.assertEqual(gh_org_client.org, expected_result)
 
-        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org}")
+        mock_get_json.assert_called_once_with(f"https://api.github.com\
+/orgs/{org}")
+
+    def test_public_repos_url(self) -> None:
+        with mock.patch('client.GithubOrgClient.org',
+                        new_callable=PropertyMock) as mock_e:
+            resp = {"repos_url": "https://api.github.com/orgs/google/repos"}
+
+            mock_e.return_value = resp
+            myclass = GithubOrgClient("google")
+            self.assertEqual(myclass._public_repos_url, resp["repos_url"])
 
 
 if __name__ == '__main__':
