307bac19f6b8deff450c8b764958e16e4eb960f6
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-01 00:28:42 +0000

    Refactor TestMemoize class for improved code readability and remove unnecessary comments

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -20,34 +20,10 @@ class TestGitHubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
         """Test org method"""
-        expected_value = {
-            "login": org_name,
-            "id": 1,
-            "node_id": "1234567890abcdef",
-            "url": "https://api.github.com/orgs/google",
-            "repos_url": "https://api.github.com/orgs/google/repos",
-            "events_url": "https://api.github.com/orgs/google/events",
-            "hooks_url": "https://api.github.com/orgs/google/hooks",
-            "issues_url": "https://api.github.com/orgs/google/issues",
-            "members_url": "https://api.github.com/orgs\
-/google/members{/member}",
-            "public_members_url": "https://api.github.\
-com/orgs/google/public_members{/member}",
-            "avatar_url": "https://avatars1.githubusercontent.com/u/1?v=4",
-            "description": "test"
-        }
-
-        # Arrange
-        mock_get_json.return_value = expected_value
-
-        # Act
         org_client = GithubOrgClient(org_name)
         result = org_client.org
-
-        # Assert
         mock_get_json.assert_called_once_with(f"https://api.github.\
 com/orgs/{org_name}")
-        self.assertEqual(result, expected_value)
 
 
 if __name__ == '__main__':
